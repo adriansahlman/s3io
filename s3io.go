4a2d7d0429d7809fe3770d39058ebd217309ed47
Adrian Sahlman <adrian.sahlman@gmail.com>
2023-05-25 17:31:05 -0400

    refactor: breaking changes

--- s3io.go
+++ s3io.go
@@ -6,12 +6,21 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/fs"
+	"net/http"
 
+	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
-var _ io.ReadSeekCloser = (*S3File)(nil)
-var _ io.ReaderAt = (*S3File)(nil)
+var (
+	ErrNotExist = fs.ErrNotExist // "file does not exist"
+)
+
+var (
+	_ io.ReadSeekCloser = (*S3File)(nil)
+	_ io.ReaderAt       = (*S3File)(nil)
+)
 
 type S3File struct {
 	ctx     context.Context
@@ -21,11 +30,11 @@ type S3File struct {
 	offset  int64
 }
 
-func NewS3File(client *s3.Client, options *s3.GetObjectInput) (*S3File, error) {
-	return NewS3FileWithContext(context.Background(), client, options)
+func OpenFile(client *s3.Client, options *s3.GetObjectInput) (*S3File, error) {
+	return OpenFileWithContext(context.Background(), client, options)
 }
 
-func NewS3FileWithContext(
+func OpenFileWithContext(
 	ctx context.Context,
 	client *s3.Client,
 	options *s3.GetObjectInput,
@@ -47,6 +56,9 @@ func NewS3FileWithContext(
 		VersionId:            options.VersionId,
 	})
 	if err != nil {
+		if isNotExist(err) {
+			err = ErrNotExist
+		}
 		return nil, err
 	}
 	return &S3File{
@@ -80,6 +92,9 @@ func (f *S3File) Read(p []byte) (n int, err error) {
 	var output *s3.GetObjectOutput
 	output, err = f.client.GetObject(f.ctx, &options)
 	if err != nil {
+		if isNotExist(err) {
+			err = ErrNotExist
+		}
 		return
 	}
 	defer func() {
@@ -88,7 +103,12 @@ func (f *S3File) Read(p []byte) (n int, err error) {
 			err = closeErr
 		}
 	}()
-	n, err = output.Body.Read(p)
+	if n, err = output.Body.Read(p); err != nil {
+		if isNotExist(err) {
+			err = ErrNotExist
+		}
+		return
+	}
 	f.offset += int64(n)
 	return
 }
@@ -122,50 +142,8 @@ func (f *S3File) ReadAt(p []byte, off int64) (n int, err error) {
 	return io.ReadFull(&cpy, p)
 }
 
-func (f *S3File) Copy(dst io.Writer) (int64, error) {
-	return f.CopyBuffer(dst, nil)
-}
-
-func (f *S3File) CopyBuffer(dst io.Writer, buf []byte) (int64, error) {
-	if f.offset >= f.size {
-		return 0, io.EOF
-	}
-	httpRange := fmt.Sprintf("bytes=%d-%d", f.offset, f.size)
-	options := *f.options
-	options.Range = &httpRange
-	output, err := f.client.GetObject(f.ctx, &options)
-	if err != nil {
-		return 0, err
-	}
-	defer func() {
-		closeErr := output.Body.Close()
-		if err == nil {
-			err = closeErr
-		}
-	}()
-	return io.CopyBuffer(dst, output.Body, buf)
-}
-
-func (f *S3File) CopyN(dst io.Writer, n int64) (int64, error) {
-	if f.offset >= f.size {
-		return 0, io.EOF
-	}
-	length := n
-	if f.offset+length > f.size {
-		length = f.size - f.offset
-	}
-	httpRange := fmt.Sprintf("bytes=%d-%d", f.offset, f.offset+length)
-	options := *f.options
-	options.Range = &httpRange
-	output, err := f.client.GetObject(f.ctx, &options)
-	if err != nil {
-		return 0, err
-	}
-	defer func() {
-		closeErr := output.Body.Close()
-		if err == nil {
-			err = closeErr
-		}
-	}()
-	return io.CopyN(dst, output.Body, n)
+func isNotExist(err error) bool {
+	var responseError *awshttp.ResponseError
+	return errors.As(err, &responseError) &&
+		responseError.ResponseError.HTTPStatusCode() == http.StatusNotFound
 }
